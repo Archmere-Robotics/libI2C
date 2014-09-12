f410e6e6f1ee6fc772d1de61ac9fc9905c0f3c5b
mailmindlin <mailmindlin@users.noreply.github.com>
2014-09-12 14:29:51 -0400

    Create Protocol.java

---
+++ rpi/Protocol.java
@@ -0,0 +1,12 @@
+public class Protocol {
+  const short pin_DATA=0x01;
+  const short pin_CLK= 0x02;
+  const short pin_REQ= 0x03;
+  const short pin_ACC= 0x04;
+  public static void setPin(short pin, boolean value) {
+    //do stuff
+  }
+  public static void send() {
+    
+  }
+}
