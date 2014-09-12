1d49aa3e9a2dc90820f4caa395f2ce1a0af59f59
mailmindlin <mailmindlin@users.noreply.github.com>
2014-09-12 14:03:10 -0400

    Update libI2C.h

--- robot/libI2C.h
+++ robot/libI2C.h
@@ -1,4 +1,13 @@
 #include "drivers/common.h"
 #include "drivers/HTPB-driver.h"
+
+// Hardware-specific support functions that MUST be customized:
+#define I2CSPEED 100
+void I2C_delay() { volatile int v; int i; for (i=0; i < I2CSPEED/2; i++) v; }
+bool read_SCL(void); // Set SCL as input and return current level of line, 0 or 1
+bool read_SDA(void); // Set SDA as input and return current level of line, 0 or 1
+void clear_SCL(void); // Actively drive SCL signal low
+void clear_SDA(void); // Actively drive SDA signal low
+void arbitration_lost(void);
 void setupI2C(short pin);
 task readI2C();
