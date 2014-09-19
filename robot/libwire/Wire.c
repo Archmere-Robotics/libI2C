e72f7047f5453e61644e11dc6ca2e6bec5f4ed2f
mailmindlin <mailmindlin@users.noreply.github.com>
2014-09-19 15:48:13 -0400

    Formatting & Fixes

--- robot/libwire/Wire.c
+++ robot/libwire/Wire.c
@@ -57,7 +57,7 @@ void wire_begin(uint8_t address) {
 }
 
 void wire_begin(int address) {
-  begin((uint8_t)address);
+  wire_begin((uint8_t)address);
 }
 
 uint8_t wire_requestFrom(uint8_t address, uint8_t quantity, uint8_t sendStop) {
@@ -75,33 +75,29 @@ uint8_t wire_requestFrom(uint8_t address, uint8_t quantity, uint8_t sendStop) {
 }
 
 uint8_t wire_requestFrom(uint8_t address, uint8_t quantity) {
-  return wire_requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)true);
+	return wire_requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)true);
 }
 
-uint8_t wire_requestFrom(int address, int quantity)
-{
-  return wire_requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)true);
+uint8_t wire_requestFrom(int address, int quantity) {
+	return wire_requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)true);
 }
 
-uint8_t wire_requestFrom(int address, int quantity, int sendStop)
-{
-  return wire_requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)sendStop);
+uint8_t wire_requestFrom(int address, int quantity, int sendStop) {
+	return wire_requestFrom((uint8_t)address, (uint8_t)quantity, (uint8_t)sendStop);
 }
 
-void wire_beginTransmission(uint8_t address)
-{
-  // indicate that we are transmitting
-  transmitting = 1;
-  // set address of targeted slave
-  txAddress = address;
-  // reset tx buffer iterator vars
-  txBufferIndex = 0;
-  txBufferLength = 0;
+void wire_beginTransmission(uint8_t address) {
+	// indicate that we are transmitting
+	transmitting = 1;
+	// set address of targeted slave
+	txAddress = address;
+	// reset tx buffer iterator vars
+	txBufferIndex = 0;
+	txBufferLength = 0;
 }
 
-void wire_beginTransmission(int address)
-{
-  wire_beginTransmission((uint8_t)address);
+void wire_beginTransmission(int address) {
+	wire_beginTransmission((uint8_t)address);
 }
 
 //
@@ -118,46 +114,45 @@ void wire_beginTransmission(int address)
 //	devices will behave oddly if they do not see a STOP.
 //
 uint8_t wire_endTransmission(uint8_t sendStop) {
-  // transmit buffer (blocking)
-  int8_t ret = twi_writeTo(txAddress, txBuffer, txBufferLength, 1, sendStop);
-  // reset tx buffer iterator vars
-  txBufferIndex = 0;
-  txBufferLength = 0;
-  // indicate that we are done transmitting
-  transmitting = 0;
-  return ret;
+	// transmit buffer (blocking)
+	int8_t ret = twi_writeTo(txAddress, txBuffer, txBufferLength, 1, sendStop);
+	// reset tx buffer iterator vars
+	txBufferIndex = 0;
+	txBufferLength = 0;
+	// indicate that we are done transmitting
+	transmitting = 0;
+	return ret;
 }
 
 //	This provides backwards compatibility with the original
 //	definition, and expected behaviour, of endTransmission
 //
 uint8_t wire_endTransmission(void) {
-  return wire_endTransmission(true);
+	return wire_endTransmission(true);
 }
 
 // must be called in:
 // slave tx event callback
 // or after beginTransmission(address)
-size_t wire_write(uint8_t data)
-{
-  if(transmitting){
-  // in master transmitter mode
-    // don't bother if buffer is full
-    if(txBufferLength >= BUFFER_LENGTH){
-      setWriteError();
-      return 0;
-    }
-    // put byte in tx buffer
-    txBuffer[txBufferIndex] = data;
-    ++txBufferIndex;
-    // update amount in buffer   
-    txBufferLength = txBufferIndex;
-  }else{
-  // in slave send mode
-    // reply to master
-    twi_transmit(&data, 1);
-  }
-  return 1;
+size_t wire_write(uint8_t data) {
+	if(transmitting){
+		// in master transmitter mode
+		// don't bother if buffer is full
+		if(txBufferLength >= BUFFER_LENGTH){
+			setWriteError();
+			return 0;
+		}
+		// put byte in tx buffer
+		txBuffer[txBufferIndex] = data;
+		++txBufferIndex;
+		// update amount in buffer   
+		txBufferLength = txBufferIndex;
+	}else{
+		// in slave send mode
+		// reply to master
+		twi_transmit(&data, 1);
+	}
+	return 1;
 }
 
 // must be called in:
@@ -181,86 +176,83 @@ size_t wire_write(const uint8_t *data, size_t quantity) {
 // slave rx event callback
 // or after requestFrom(address, numBytes)
 int wire_available(void) {
-  return rxBufferLength - rxBufferIndex;
+	return rxBufferLength - rxBufferIndex;
 }
 
 // must be called in:
 // slave rx event callback
 // or after requestFrom(address, numBytes)
 int TwoWire::read(void) {
-  int value = -1;
+	int value = -1;
   
-  // get each successive byte on each call
-  if(rxBufferIndex < rxBufferLength){
-    value = rxBuffer[rxBufferIndex];
-    ++rxBufferIndex;
-  }
-
-  return value;
+	// get each successive byte on each call
+	if(rxBufferIndex < rxBufferLength){
+		value = rxBuffer[rxBufferIndex];
+		++rxBufferIndex;
+	}
+	return value;
 }
 
 // must be called in:
 // slave rx event callback
 // or after requestFrom(address, numBytes)
 int wire_peek(void) {
-  int value = -1;
-  
-  if(rxBufferIndex < rxBufferLength){
-    value = rxBuffer[rxBufferIndex];
-  }
-
-  return value;
+	int value = -1;
+	
+	if(rxBufferIndex < rxBufferLength){
+		value = rxBuffer[rxBufferIndex];
+	}
+	return value;
 }
 
 void wire_flush(void) {
-  // XXX: to be implemented.
+	// XXX: to be implemented.
 }
 
 // behind the scenes function that is called when data is received
 void wire_onReceiveService(uint8_t* inBytes, int numBytes) {
-  // don't bother if user hasn't registered a callback
-  if(!user_onReceive){
-    return;
-  }
-  // don't bother if rx buffer is in use by a master requestFrom() op
-  // i know this drops data, but it allows for slight stupidity
-  // meaning, they may not have read all the master requestFrom() data yet
-  if(rxBufferIndex < rxBufferLength){
-    return;
-  }
-  // copy twi rx buffer into local read buffer
-  // this enables new reads to happen in parallel
-  for(uint8_t i = 0; i < numBytes; ++i){
-    rxBuffer[i] = inBytes[i];    
-  }
-  // set rx iterator vars
-  rxBufferIndex = 0;
-  rxBufferLength = numBytes;
-  // alert user program
-  user_onReceive(numBytes);
+	// don't bother if user hasn't registered a callback
+	if(!user_onReceive){
+		return;
+	}
+	// don't bother if rx buffer is in use by a master requestFrom() op
+	// i know this drops data, but it allows for slight stupidity
+	// meaning, they may not have read all the master requestFrom() data yet
+	if(rxBufferIndex < rxBufferLength){
+		return;
+	}
+	// copy twi rx buffer into local read buffer
+	// this enables new reads to happen in parallel
+	for(uint8_t i = 0; i < numBytes; ++i){
+		rxBuffer[i] = inBytes[i];    
+	}
+	// set rx iterator vars
+	rxBufferIndex = 0;
+	rxBufferLength = numBytes;
+	// alert user program
+	user_onReceive(numBytes);
 }
 
 // behind the scenes function that is called when data is requested
 void wire_onRequestService(void) {
-  // don't bother if user hasn't registered a callback
-  if(!user_onRequest){
-    return;
-  }
-  // reset tx buffer iterator vars
-  // !!! this will kill any pending pre-master sendTo() activity
-  txBufferIndex = 0;
-  txBufferLength = 0;
-  // alert user program
-  user_onRequest();
+	// don't bother if user hasn't registered a callback
+	if(!user_onRequest){
+		return;
+	}
+	// reset tx buffer iterator vars
+	// !!! this will kill any pending pre-master sendTo() activity
+	txBufferIndex = 0;
+	txBufferLength = 0;
+	// alert user program
+	user_onRequest();
 }
 
 // sets function called on slave write
 void wire_onReceive( void (*function)(int) ) {
-  user_onReceive = function;
+	user_onReceive = function;
 }
 
 // sets function called on slave read
-void wire_onRequest( void (*function)(void) )
-{
-  user_onRequest = function;
+void wire_onRequest( void (*function)(void) ) {
+	user_onRequest = function;
 }
